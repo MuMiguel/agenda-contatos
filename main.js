3550a32ce96c4700508c50d2125a778117b93853
Murilo Miguel <murilo.miguel0801@gmail.com>
2023-09-04 19:45:46 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('formAgenda');
+const nomes = [];
+const numeros = [];
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinhas();
+    atualizaTabela();
+});
+    
+
+function adicionaLinhas () {
+    const inputNomeContato = document.getElementById('nomeContato');
+    const inputTeleContato = document.getElementById('telContato');
+
+    if(nomes.includes(inputNomeContato.value)) {
+        alert(`O contato "${inputNomeContato.value}" já foi inserido`);
+    }else {
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputTeleContato.value}</td>`;
+        linhas += linha;
+
+        nomes.push(inputNomeContato.value);
+        numeros.push(inputTeleContato.value);
+
+        inputNomeContato.value = '';
+        inputTeleContato.value = '';
+    }
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+
+
